0c29cd11b3272f10a29431868abdc1384e16f47a
Yu Zhang <yzhang.cs@outlook.com>
2025-01-23 16:40:57 +0000

    [RetNet] Rename `offsets` to `cu_seqlens`

--- fla/ops/retention/fused_recurrent.py
+++ fla/ops/retention/fused_recurrent.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2024, Songlin Yang, Yu Zhang
+# Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 
 from typing import Optional, Tuple
+
 import torch
+
 from fla.ops.simple_gla.fused_recurrent import fused_recurrent_simple_gla
 
 
@@ -14,14 +16,14 @@ def fused_recurrent_retention(
     initial_state: Optional[torch.Tensor] = None,
     output_final_state: bool = False,
     reverse: bool = False,
-    offsets: Optional[torch.LongTensor] = None,
+    cu_seqlens: Optional[torch.LongTensor] = None,
     head_first: bool = True
 ) -> Tuple[torch.Tensor, torch.Tensor]:
     if head_first:
         n_heads = q.shape[1]
     else:
         n_heads = q.shape[2]
-    s = (1 - q.new_tensor(2., dtype=torch.float, device=q.device).pow(-5. - q.new_tensor(range(n_heads), dtype=torch.float, device=q.device))).log()
+    s = (1 - q.new_tensor(2., dtype=torch.float).pow(-5. - q.new_tensor(range(n_heads), dtype=torch.float))).log()
     if head_first:
         g = s[None, :, None].expand(q.shape[0], q.shape[1], q.shape[2]).contiguous()
     else:
@@ -35,6 +37,6 @@ def fused_recurrent_retention(
         initial_state=initial_state,
         output_final_state=output_final_state,
         reverse=reverse,
-        cu_seqlens=offsets,
+        cu_seqlens=cu_seqlens,
         head_first=head_first
     )
--- fla/ops/retention/parallel.py
+++ fla/ops/retention/parallel.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2024, Songlin Yang, Yu Zhang
+# Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 
 from typing import Optional, Tuple
 
@@ -46,8 +46,7 @@ def parallel_retention(
         n_heads = q.shape[1]
     else:
         n_heads = q.shape[2]
-    s = (1 - q.new_tensor(2., dtype=torch.float, device=q.device).pow(-5. -
-         q.new_tensor(range(n_heads), dtype=torch.float, device=q.device))).log()
+    s = (1 - q.new_tensor(2., dtype=torch.float).pow(-5. - q.new_tensor(range(n_heads), dtype=torch.float))).log()
     if head_first:
         g = s[None, :, None].expand(q.shape[0], q.shape[1], q.shape[2]).contiguous()
     else:
