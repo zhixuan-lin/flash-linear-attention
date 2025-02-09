33bf7dbff48c9b840f51e55e74f39946dfc2f049
Yu Zhang <yzhang.cs@outlook.com>
2025-02-09 08:09:12 -0800

    [HGRN2] Fix missing config

--- fla/models/hgrn2/modeling_hgrn2.py
+++ fla/models/hgrn2/modeling_hgrn2.py
@@ -32,7 +32,9 @@ logger = logging.get_logger(__name__)
 class HGRN2Block(nn.Module):
     def __init__(self, config: HGRN2Config, layer_idx: int):
         super().__init__()
-        self.hidden_size = config.hidden_size
+
+        self.config = config
+        self.layer_idx = layer_idx
 
         self.attn_norm = (RMSNorm if config.fuse_norm else nn.RMSNorm)(config.hidden_size, eps=config.norm_eps)
         if config.attn is not None and layer_idx in config.attn['layers']:
