d75f28fe0436f7a2d46d61ccad4f9ab7cc9948dc
Yu Zhang <yzhang.cs@outlook.com>
2025-02-10 07:10:05 -0800

    [RWKV] Avoid conflicts with hf definitions

--- fla/models/rwkv7/__init__.py
+++ fla/models/rwkv7/__init__.py
@@ -5,9 +5,9 @@ from transformers import AutoConfig, AutoModel, AutoModelForCausalLM
 from fla.models.rwkv7.configuration_rwkv7 import RWKV7Config
 from fla.models.rwkv7.modeling_rwkv7 import RWKV7ForCausalLM, RWKV7Model
 
-AutoConfig.register(RWKV7Config.model_type, RWKV7Config)
-AutoModel.register(RWKV7Config, RWKV7Model)
-AutoModelForCausalLM.register(RWKV7Config, RWKV7ForCausalLM)
+AutoConfig.register(RWKV7Config.model_type, RWKV7Config, True)
+AutoModel.register(RWKV7Config, RWKV7Model, True)
+AutoModelForCausalLM.register(RWKV7Config, RWKV7ForCausalLM, True)
 
 
 __all__ = ['RWKV7Config', 'RWKV7ForCausalLM', 'RWKV7Model']
