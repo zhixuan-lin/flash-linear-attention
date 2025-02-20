adc931db1cbe978f73b2c3314784a7fcf3a6de4b
Yu Zhang <yzhang.cs@outlook.com>
2025-02-20 18:56:33 +0000

    [Utils] Add fn for preparing seq idx

--- fla/ops/common/utils.py
+++ fla/ops/common/utils.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 
-from typing import Tuple
-
 import torch
 import triton
 
@@ -10,10 +8,17 @@ from fla.utils import tensor_cache
 
 
 @tensor_cache
+def prepare_sequence_indices(offsets: torch.LongTensor) -> torch.LongTensor:
+    indices = torch.cat([torch.arange(n) for n in (offsets[1:] - offsets[:-1]).tolist()])
+    indices = torch.stack([indices.eq(0).cumsum(0) - 1, indices], 1).to(offsets)
+    return indices
+
+
+@tensor_cache
 def prepare_chunk_offsets(
     offsets: torch.Tensor,
     chunk_size: int
-) -> Tuple[torch.Tensor, torch.Tensor]:
+) -> torch.LongTensor:
     return torch.cat([offsets.new_tensor([0]), triton.cdiv(offsets[1:] - offsets[:-1], chunk_size)]).cumsum(-1)
 
 
@@ -21,7 +26,7 @@ def prepare_chunk_offsets(
 def prepare_chunk_indices(
     offsets: torch.LongTensor,
     chunk_size: int
-) -> Tuple[torch.LongTensor]:
+) -> torch.LongTensor:
     indices = torch.cat([torch.arange(n) for n in triton.cdiv(offsets[1:] - offsets[:-1], chunk_size).tolist()])
     indices = torch.stack([indices.eq(0).cumsum(0) - 1, indices], 1).to(offsets)
     return indices
