6894afb1e0d7cde916677feeb1507802b86d44c4
Yu Zhang <yzhang.cs@outlook.com>
2025-03-20 13:08:09 -0700

    [Benchmarks] Delete naive RetNet cols

--- benchmarks/ops/benchmark_retention.py
+++ benchmarks/ops/benchmark_retention.py
@@ -5,14 +5,9 @@ import os
 import torch
 import triton
 
-from fla.ops.retention import chunk_retention, fused_recurrent_retention, parallel_retention
-from fla.ops.retention.naive import naive_retention
+from fla.ops.retention import chunk_retention, parallel_retention
 
-try:
-    from flash_attn import flash_attn_func
-    HAS_FLASH = True
-except BaseException:
-    HAS_FLASH = False
+from flash_attn import flash_attn_func
 
 
 @triton.testing.perf_report(
@@ -24,14 +19,11 @@ except BaseException:
         # argument name whose value corresponds to a different line in the plot
         line_arg='provider',
         # possible values for `line_arg``
-        line_vals=['fused_chunk', 'chunk', 'parallel',
-                   'chunk_bwd', 'parallel_bwd'] + (['flash', 'flash_bwd'] if HAS_FLASH else []),
+        line_vals=['chunk', 'parallel', 'flash', 'chunk_bwd', 'parallel_bwd', 'flash_bwd'],
         # label name for the lines
-        line_names=['fused_chunk_fwd', 'chunk_fwd', 'parallel_fwd',
-                    'chunk_fwdbwd', 'parallel_fwdbwd'] + (['flash_fwd', 'flash_fwdbwd'] if HAS_FLASH else []),
+        line_names=['chunk_fwd', 'parallel_fwd', 'flash_fwd', 'chunk_fwdbwd', 'parallel_fwdbwd', 'flash_fwdbwd'],
         # line styles
-        styles=[('green', '-'), ('blue', '-'), ('red', '-'), ('green', 'dotted'), ('blue', 'dotted'),
-                ('red', 'dotted')] + ([('cyan', '-'), ('cyan', 'dotted')] if HAS_FLASH else []),
+        styles=[('green', '-'), ('blue', '-'), ('red', '-'), ('green', 'dotted'), ('blue', 'dotted'), ('red', 'dotted')],
         ylabel="Execution Time (ms)",  # label name for the y-axis
         # name for the plot. Used also as a file name for saving the plot.
         plot_name="Performance",
@@ -57,32 +49,20 @@ def benchmark(T, provider):
 
     quantiles = [0.5, 0.2, 0.8]
     results = 0, 0, 0
-    if provider == 'torch':
-        if T > 2048:
-            return results
-        results = triton.testing.do_bench(lambda: naive_retention(q, k, v), quantiles=quantiles)
-    elif provider == 'recurrent':
-        results = triton.testing.do_bench(lambda: fused_recurrent_retention(q, k, v), quantiles=quantiles)
-    elif provider == 'chunk':
+    if provider == 'chunk':
         results = triton.testing.do_bench(lambda: chunk_retention(q, k, v), quantiles=quantiles)
     elif provider == 'parallel':
         results = triton.testing.do_bench(lambda: parallel_retention(q, k, v), quantiles=quantiles)
-    elif provider == 'torch_bwd':
-        if T > 2048:
-            return results
-        results = triton.testing.do_bench(lambda: naive_retention(q, k, v).backward(do), quantiles=quantiles)
-    elif provider == 'recurrent_bwd':
-        results = triton.testing.do_bench(lambda: fused_recurrent_retention(q, k, v)[0].backward(do), quantiles=quantiles)
+    elif provider == 'flash':
+        results = triton.testing.do_bench(lambda: flash_attn_func(q, k, v, causal=True), quantiles=quantiles)
     elif provider == 'chunk_bwd':
         results = triton.testing.do_bench(lambda: chunk_retention(q, k, v)[0].backward(do), quantiles=quantiles)
     elif provider == 'parallel_bwd':
         results = triton.testing.do_bench(lambda: parallel_retention(q, k, v)[0].backward(do), quantiles=quantiles)
-    elif provider == 'flash':
-        results = triton.testing.do_bench(lambda: flash_attn_func(q, k, v, causal=True), quantiles=quantiles)
     elif provider == 'flash_bwd':
         results = triton.testing.do_bench(lambda: flash_attn_func(q, k, v, causal=True).backward(do), quantiles=quantiles)
     return results
 
 
 if __name__ == '__main__':
-    benchmark.run(print_data=True)
+    benchmark.run(print_data=True, save_path='.')
