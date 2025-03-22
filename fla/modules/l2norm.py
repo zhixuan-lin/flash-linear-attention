7e385e1edc41ac9071cd3092f5de7151b9df4eaa
Yu Zhang <yzhang.cs@outlook.com>
2025-03-22 13:30:02 -0700

    [Misc.] Reuse masks in l2norm kernels

--- fla/modules/l2norm.py
+++ fla/modules/l2norm.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 
 from typing import Optional
 
@@ -14,30 +15,28 @@ from fla.utils import input_guard
         triton.Config({}, num_warps=num_warps)
         for num_warps in [1, 2, 4, 8, 16, 32]
     ],
-    key=["N"]
+    key=['N']
 )
 @triton.jit
 def l2norm_fwd_kernel(
-    X,  # pointer to the input
-    Y,  # pointer to the output
-    stride_x_row,  # how much to increase the pointer when moving by 1 row
-    N,  # number of columns in X
-    eps,  # epsilon to avoid division by zero
+    X,
+    Y,
+    N,
+    eps,
     BLOCK_N: tl.constexpr,
 ):
-    # Map the program id to the row of X and Y it should compute.
-    row = tl.program_id(0)
-    X += row * stride_x_row
-    Y += row * stride_x_row
+    i_m = tl.program_id(0)
+    X += i_m * N
+    Y += i_m * N
     # Compute mean and variance
     cols = tl.arange(0, BLOCK_N)
-    x = tl.load(X + cols, mask=cols < N, other=0.0).to(tl.float32)
-    xbar = tl.where(cols < N, x, 0.0)
+    mask = cols < N
+    x = tl.load(X + cols, mask=mask, other=0.0).to(tl.float32)
+    xbar = tl.where(mask, x, 0.0)
     var = tl.sum(xbar * xbar, axis=0)
     rstd = 1 / tl.sqrt(var + eps)
-    # tl.store(Rstd + row, rstd)
+    # tl.store(Rstd + i_m, rstd)
     # Normalize and apply linear transformation
-    mask = cols < N
     y = x * rstd
     # Write output
     tl.store(Y + cols, y, mask=mask)
@@ -48,43 +47,34 @@ def l2norm_fwd_kernel(
         triton.Config({}, num_warps=num_warps)
         for num_warps in [1, 2, 4, 8, 16, 32]
     ],
-    key=["N"],
+    key=['N']
 )
-# @triton.heuristics({"HAS_BIAS": lambda args: args["B"] is not None})
-# @triton.heuristics({"HAS_DRESIDUAL": lambda args: args["DRESIDUAL"] is not None})
-# @triton.heuristics({"STORE_DRESIDUAL": lambda args: args["DRESIDUAL_IN"] is not None})
-# @triton.heuristics({"RECOMPUTE_OUTPUT": lambda args: args["Y"] is not None})
 @triton.jit
 def l2norm_bwd_kernel(
-    X,  # pointer to the input
-    # Y, # pointer to the output to be recomputed
-    DY,  # pointer to the output gradient
-    DX,  # pointer to the input gradient
-    stride_x_row,  # how much to increase the pointer when moving by 1 row
-    N,  # number of columns in X
-    eps,  # epsilon to avoid division by zero
+    X,
+    DY,
+    DX,
+    N,
+    eps,
     BLOCK_N: tl.constexpr,
 ):
-    # Map the program id to the elements of X, DX, and DY it should compute.
-    # Map the program id to the row of X and Y it should compute.
-    row = tl.program_id(0)
-    X += row * stride_x_row
-    DX += row * stride_x_row
-    DY += row * stride_x_row
+    i_m = tl.program_id(0)
+    X += i_m * N
+    DX += i_m * N
+    DY += i_m * N
 
-    # Y += row * stride_y_row
+    # Y += i_m * stride_y_row
     cols = tl.arange(0, BLOCK_N)
-    x = tl.load(X + cols, mask=cols < N, other=0.0).to(tl.float32)
-    x = tl.where(cols < N, x, 0.0)
+    mask = cols < N
+    x = tl.load(X + cols, mask=mask, other=0.0).to(tl.float32)
+    x = tl.where(mask, x, 0.0)
     var = tl.sum(x * x)
     rstd = 1 / tl.sqrt(var + eps)
-    # tl.store(Rstd + row, rstd)
+    # tl.store(Rstd + i_m, rstd)
     # Normalize and apply linear transformation
-    mask = cols < N
     # y = x * rstd
-    dy = tl.load(DY + cols, mask=cols < N, other=0.0).to(tl.float32)
-    dy = tl.where(cols < N, dy, 0.0)
-    # dx = dy * rstd - tl.sum(dy * x) * (1 / (var+eps)) * rstd * x
+    dy = tl.load(DY + cols, mask=mask, other=0.0).to(tl.float32)
+    dy = tl.where(mask, dy, 0.0)
     dx = dy * rstd - tl.sum(dy * x) * (1 / (var+eps)) * rstd * x
     tl.store(DX + cols, dx, mask=mask)
 
@@ -96,10 +86,6 @@ def l2norm_fwd(
 ):
     x_shape_og = x.shape
     x = x.reshape(-1, x.shape[-1])
-    if x.stride(-1) != 1:
-        x = x.contiguous()
-        M, N = x.shape
-    assert x.stride(-1) == 1
     # allocate output
     if output_dtype is None:
         y = torch.empty_like(x)
@@ -113,20 +99,14 @@ def l2norm_fwd(
     MAX_FUSED_SIZE = 65536 // x.element_size()
     BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
     if N > BLOCK_N:
-        raise RuntimeError(
-            "This layer norm doesn't support feature dim >= 64KB.")
+        raise RuntimeError("This layer norm doesn't support feature dim >= 64KB.")
     # heuristics for number of warps
     l2norm_fwd_kernel[(M,)](
         x,
         y,
-        x.stride(0),
         N,
         eps,
-        # is_rms_norm,
         BLOCK_N,
-        # residual is not None,
-        # residual_out is not None,
-        # bias is not None,
     )
     return y.reshape(x_shape_og)
 
@@ -144,23 +124,19 @@ def l2norm_bwd(
     assert dy.shape == x.shape
     # allocate output
     dx = torch.empty_like(x)
-    N = x.shape[-1]
     M = x.shape[0]
-    assert x.stride(-1) == 1
-    assert dy.stride(-1) == 1
+    N = x.shape[-1]
     # rstd = torch.empty((M,), dtype=torch.float32, device=x.device)
     # Less than 64KB per feature: enqueue fused kernel
     MAX_FUSED_SIZE = 65536 // x.element_size()
     BLOCK_N = min(MAX_FUSED_SIZE, triton.next_power_of_2(N))
     if N > BLOCK_N:
-        raise RuntimeError(
-            "This layer norm doesn't support feature dim >= 64KB.")
+        raise RuntimeError("This layer norm doesn't support feature dim >= 64KB.")
     # heuristics for number of warps
     l2norm_bwd_kernel[(M,)](
         x,
         dy,
         dx,
-        x.stride(0),
         N,
         eps,
         BLOCK_N,
@@ -170,24 +146,23 @@ def l2norm_bwd(
 
 class L2NormFunction(torch.autograd.Function):
 
-    @input_guard
     @staticmethod
+    @input_guard
     def forward(
         ctx,
         x,
         eps=1e-6,
         output_dtype=None
     ):
-        # reshape input data into 2D tensor
         y = l2norm_fwd(x, eps, output_dtype)
         ctx.eps = eps
         ctx.x_dtype = x.dtype
         ctx.save_for_backward(x)
         return y
 
-    @input_guard
     @staticmethod
-    def backward(ctx, dy, *args):
+    @input_guard
+    def backward(ctx, dy):
         x, = ctx.saved_tensors
         dx = l2norm_bwd(x, dy, ctx.eps)
         return dx, None, None
