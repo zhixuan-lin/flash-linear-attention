67ca03a9ad7d1d894660f81848984ad8bf96c4fb
Yu Zhang <yzhang.cs@outlook.com>
2025-03-28 18:03:20 +0800

    [Tests] Add cases for shortconv varlen inference

--- tests/modules/test_conv.py
+++ tests/modules/test_conv.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 
 from fla.modules.convolution import ShortConvolution
-from fla.ops.common.utils import prepare_position_ids, prepare_sequence_ids
+from fla.ops.common.utils import prepare_lens, prepare_position_ids, prepare_sequence_ids
 from fla.ops.utils.testing import assert_close
 from fla.utils import device
 
@@ -52,7 +52,7 @@ def test_shortconv_varlen(N: int, T: int, H: int, C: int):
     conv = ShortConvolution(H, C, activation='silu', use_fast_conv1d=True).to(device)
     offsets = torch.cat([
         torch.tensor([0], dtype=torch.long),
-        torch.arange(16, T)[torch.randperm(T - 1)[:N-1]],
+        torch.arange(16, T)[torch.randperm(T - 16)[:N-1]],
         torch.tensor([T], dtype=torch.long)
     ], 0).to(device).sort()[0]
 
@@ -87,6 +87,40 @@ def test_shortconv_cache(B: int, T: int, H: int, C: int):
     for i in range(T):
         y_slow, cache_slow = conv_slow(x[:, i:i+1], mask=mask[:, i:i+1], cache=cache_slow, output_final_state=True)
         y_fast, cache_fast = conv_fast(x[:, i:i+1], mask=mask[:, i:i+1], cache=cache_fast, output_final_state=True)
+
+        assert_close(f" slow {i:2}", y_slow, y[:, i:i+1], 1e-5)
+        assert_close(f" fast {i:2}", y_fast, y[:, i:i+1], 1e-5)
+        assert_close(f"cache {i:2}", cache_slow, cache_fast, 1e-5)
+
+
+@pytest.mark.parametrize("N", [4])
+@pytest.mark.parametrize("T", [500, 1024, 2048])
+@pytest.mark.parametrize("H", [128])
+@pytest.mark.parametrize("C", [4])
+@pytest.mark.skipif(
+    causal_conv1d_fn is None,
+    reason="causal_conv1d is not installed"
+)
+def test_shortconv_cache_varlen(N: int, T: int, H: int, C: int):
+    torch.manual_seed(42)
+    conv_slow = ShortConvolution(H, C, use_fast_conv1d=False).to(device)
+    conv_fast = ShortConvolution(H, C, use_fast_conv1d=True).to(device)
+    conv_fast.weight.data.copy_(conv_slow.weight.data)
+    if conv_fast.bias is not None:
+        conv_fast.bias.data.copy_(conv_slow.bias.data)
+    offsets = torch.cat([
+        torch.tensor([0], dtype=torch.long),
+        torch.arange(16, T)[torch.randperm(T - 16)[:N-1]],
+        torch.tensor([T], dtype=torch.long)
+    ], 0).to(device).sort()[0]
+
+    x = torch.randn(1, T, H).to(device)
+    seq_idx = prepare_sequence_ids(prepare_position_ids(offsets)).to(torch.int32).unsqueeze(0)
+    y, _ = conv_slow(x, seq_idx=seq_idx)
+    cache_slow, cache_fast = None, None
+    for i in range(min(prepare_lens(offsets))):
+        y_slow, cache_slow = conv_slow(x[:, i:i+1], cache=cache_slow, output_final_state=True)
+        y_fast, cache_fast = conv_fast(x[:, i:i+1], cache=cache_fast, output_final_state=True)
 
         assert_close(f" slow {i:2}", y_slow, y[:, i:i+1], 1e-5)
         assert_close(f" fast {i:2}", y_fast, y[:, i:i+1], 1e-5)
